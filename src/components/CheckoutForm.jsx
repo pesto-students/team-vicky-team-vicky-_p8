47456739dd2007309451870ed64985662d5794ea
Kushal D Jain <jkushal069@gmail.com>
2023-07-15 17:18:49 +0530

    Update CheckoutForm.jsx

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -66,7 +66,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/success",
+        return_url: "https://team-vicky-team-vicky-p8.vercel.app/success",
       },
     });
 
